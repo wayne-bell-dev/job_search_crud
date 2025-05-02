5b4478ee7869071bef7e12a45c4b0e7e18bac4c6
Wayne Bell <wayne.bell.dev@gmail.com>
2025-05-02 19:13:08 -0400

    chore: initialize Spring Boot project with JPA and MySQL dependencies

---
+++ src/main/java/com/bellwe/jobsearchcrud/JobSearchCrudApplication.java
@@ -0,0 +1,13 @@
+package com.bellwe.jobsearchcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSearchCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bellwe/jobsearchcrud/JobSearchCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellwe.jobsearchcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
