798d87082c925bfc024c5acc0a96501b8c625ebf
Wayne Bell <wayne.bell.dev@gmail.com>
2025-05-10 19:08:39 -0400

    feat: add dto classes for CRUD operations

---
+++ src/main/java/com/bellwe/jobsearchcrud/dto/CreateJobApplicationDTO.java
@@ -0,0 +1,65 @@
+package com.bellwe.jobsearchcrud.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
+
+public class CreateJobApplicationDTO {
+
+    @NotBlank
+    private String jobTitle;
+
+    @NotBlank
+    private String company;
+
+    @PastOrPresent
+    private LocalDate dateApplied;
+
+    @Length(max = 100)
+    private String status;
+
+    @Length(max = 1000)
+    private String notes;
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public LocalDate getDateApplied() {
+        return dateApplied;
+    }
+
+    public void setDateApplied(LocalDate dateApplied) {
+        this.dateApplied = dateApplied;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
---
+++ src/main/java/com/bellwe/jobsearchcrud/dto/JobApplicationResponseDTO.java
@@ -0,0 +1,66 @@
+package com.bellwe.jobsearchcrud.dto;
+
+import java.time.LocalDate;
+
+public class JobApplicationResponseDTO {
+
+    private int id;
+
+    private String jobTitle;
+
+    private String company;
+
+    private LocalDate dateApplied;
+
+    private String status;
+
+    private String notes;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public LocalDate getDateApplied() {
+        return dateApplied;
+    }
+
+    public void setDateApplied(LocalDate dateApplied) {
+        this.dateApplied = dateApplied;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
